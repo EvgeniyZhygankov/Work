1e18350681ea4486b709e982004331a0cfef64ad
Евгений Цыганков <valeriysemenovich1958@gmail.com>
2019-10-09 00:08:41 +0300

    anther commit

--- script.js
+++ script.js
@@ -3,34 +3,31 @@ window.onload = qwe();
 window.addEventListener("resize", qwe);
 
 function qwe() {
-    // alert(`Ширина ${window.innerWidth}
-    //        Высота ${window.innerHeight}`);
-    console.log("in")
-    let SRCsSmall = [
-        "images/slide-man-small.png",
-        "images/slide-women-small.png"
-    ];
-    let SRCsNormal = [
-        "images/slide-man.png",
-        "images/slide-women.png",
-        "images/chin.png"
-    ];
-    let carousel_inner = document.querySelector(".carousel-inner");
-    let imgs = document.querySelectorAll(".d-block");
+    console.log(`Ширина ${window.innerWidth} Высота ${window.innerHeight}`);
+    // let SRCsSmall = [
+    //     "images/slide-man-small.png",
+    //     "images/slide-women-small.png"
+    // ];
+    // let SRCsNormal = [
+    //     "images/slide-man.png",
+    //     "images/slide-women.png",
+    //     "images/chin.png"
+    // ];
+    // let carousel_inner = document.querySelector(".carousel-inner");
+    // let imgs = document.querySelectorAll(".d-block");
     
-    console.log(window.innerWidth);
-    if (window.innerWidth < 1900) {
-        console.log(imgs);
-        imgs[0].setAttribute("src", SRCsSmall[0]);
-        imgs[1].setAttribute("src", SRCsSmall[1]);
-        carousel_inner.childNodes[2].className = "";
-    }
-    else {
-        imgs.forEach((img, index) => {
+    // if (window.innerWidth < 1900) {
+    //     console.log(imgs);
+    //     imgs[0].setAttribute("src", SRCsSmall[0]);
+    //     imgs[1].setAttribute("src", SRCsSmall[1]);
+    //     carousel_inner.childNodes[2].className = "";
+    // }
+    // else {
+    //     imgs.forEach((img, index) => {
 
-            img.setAttribute("src", SRCsNormal[index]);
-        });
+    //         img.setAttribute("src", SRCsNormal[index]);
+    //     });
 
-        carousel_inner.childNodes[2].className = "carousel-item";
-    }
+    //     carousel_inner.childNodes[2].className = "carousel-item";
+    // }
 }
